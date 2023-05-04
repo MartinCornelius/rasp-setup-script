e955859cfe3eff06505b13496762e0a14a2a2e59
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-05-04 11:47:02 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,7 +54,7 @@ fun message_callback (text) {
 
 Plymouth.SetUpdateStatusFunction(message_callback);" | sudo tee /usr/share/plymouth/themes/pix/pix.script
 
-echo " quiet splash plymouth.ignore-serial-consoles logo.nologo vt.global_cursor_default=0" | sudo tee -a /boot/cmdline.txt
+echo " quiet splash plymouth.ignore-serial-consoles logo.nologo vt.global_cursor_default=0 splash-delay=5" | sudo tee -a /boot/cmdline.txt
 
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 sudo apt-get install --no-install-recommends chromium-browser -y
