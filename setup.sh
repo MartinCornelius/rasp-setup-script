be259e31921ed855d78ffd8598ce67d568af2e16
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-05-04 09:33:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-echo 'Installing need packages...'
+echo 'Starting setup process'
 sudo apt-get update
 sudo apt-get install lightdm -y
-sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
-sudo apt-get install --no-install-recommends chromium-browser -y
 sudo apt-get install plymouth plymouth-themes -y
 sudo apt-get install pix-plym-splash -y
 
-echo 'Changing some things...'
-sudo printf "xset s off\nxset s noblank\nxset -dpms\nchromium-browser --disable-infobars --kiosk '%s'" $1 > /etc/xdg/openbox/autostart
-sudo printf "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor" > .bash_profile
 sudo mv splash.png /usr/share/plymouth/themes/pix
-
 echo "disable_splash=1" | sudo tee -a /boot/config.txt
-echo " quiet splash plymouth.ignore-serial-consoles logo.nologo vt.global_cursor_default=0" | sudo tee -a /boot/cmdline.txt
 
 sudo echo "screen_width = Window.GetWidth();
 screen_height = Window.GetHeight();
@@ -60,5 +53,12 @@ fun message_callback (text) {
 }
 
 Plymouth.SetUpdateStatusFunction(message_callback);" | sudo tee /usr/share/plymouth/themes/pix/pix.script
+
+echo " quiet splash plymouth.ignore-serial-consoles logo.nologo vt.global_cursor_default=0" | sudo tee -a /boot/cmdline.txt
+
+sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
+sudo apt-get install --no-install-recommends chromium-browser -y
+
+sudo printf "xset s off\nxset s noblank\nxset -dpms\nsetxkbmap -option terminate:ctrl_alt_bksp\nsed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' ~/.config/chromium/'Local State'\nsed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/; s/\"exit_type\":\"[^\"]\\+\"/\"exit_type\":\"Normal\"/' ~/.config/chromium/Default/Preferences\nchromium-browser --disable-infobars --kiosk '%s'" $1 > /etc/xdg/openbox/autostart
 
 echo 'Done. Go edit raspi-config...'
