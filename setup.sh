988cc7dc24eeb2bfca3f83d4359c178040ea6db3
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-05-09 23:13:29 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,67 +8,24 @@ fi
 echo 'Starting setup process'
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install lightdm -y
-sudo raspi-config
-sudo apt-get install plymouth plymouth-themes -y
-sudo apt-get install pix-plym-splash -y
 
-sudo mv splash.png /usr/share/plymouth/themes/pix
 echo "disable_splash=1" | sudo tee -a /boot/config.txt
 
-sudo echo "screen_width = Window.GetWidth();
-screen_height = Window.GetHeight();
+echo " logo.nologo consoleblank=0 loglevel=1 quiet vt.global_cursor_default=0" | sudo tee -a /boot/cmdline.txt
 
-theme_image = Image("splash.png");
-image_width = theme_image.GetWidth();
-image_height = theme_image.GetHeight();
+sudo systemctl disable getty@tty3
+sudo apt-get install fbi -y
 
-scale_x = image_width / screen_width;
-scale_y = image_height / screen_height;
-
-flag = 1;
-
-if (scale_x > 1 || scale_y > 1)
-{
-        if (scale_x > scale_y)
-        {
-                resized_image = theme_image.Scale (screen_width, image_height / scale_x);
-                image_x = 0;
-                image_y = (screen_height - ((image_height  * screen_width) / image_width)) / 2;
-        }
-        else
-        {
-                resized_image = theme_image.Scale (image_width / scale_y, screen_height);
-                image_x = (screen_width - ((image_width  * screen_height) / image_height)) / 2;
-                image_y = 0;
-        }
-}
-else
-{
-        resized_image = theme_image.Scale (image_width, image_height);
-        image_x = (screen_width - image_width) / 2;
-        image_y = (screen_height - image_height) / 2;
-}
-
-if (Plymouth.GetMode() != "shutdown")
-{
-        sprite = Sprite (resized_image);
-        sprite.SetPosition (image_x, image_y, -100);
-}
-
-fun message_callback (text) {
-        sprite.SetImage (resized_image);
-}
-
-Plymouth.SetUpdateStatusFunction(message_callback);" | sudo tee /usr/share/plymouth/themes/pix/pix.script
-
-echo " quiet splash plymouth.ignore-serial-consoles logo.nologo vt.global_cursor_default=0" | sudo tee -a /boot/cmdline.txt
+sudo printf "[Unit]\nDescription=Splash screen\nDefaultDependencies=no\nAfter=local-fs.target\n\n[Service]\nExecStartPre=/usr/bin/sleep 2\nExecStart=/usr/bin/fbi -d /dev/fb0 --noverbose -a /home/%s/splash.png\nStandardInput=tty\nStandardOutput=tty\n\n[Install]\nWantedBy=sysinit.target" $1 | sudo tee /etc/systemd/system/splashscreen.service
+sudo systemctl enable splashscreen
 
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 sudo apt-get install --no-install-recommends chromium-browser -y
 
-sudo printf "xset s off\nxset s noblank\nxset -dpms\nsetxkbmap -option terminate:ctrl_alt_bksp\nsed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' ~/.config/chromium/'Local State'\nsed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/; s/\"exit_type\":\"\[^\"\]\\+\"/\"exit_type\":\"Normal\"/' ~/.config/chromium/Default/Preferences\nchromium-browser --disable-infobars --kiosk '%s'" $1 | sudo tee /etc/xdg/openbox/autostart
+sudo printf "xset s off\nxset s noblank\nxset -dpms\nsetxkbmap -option terminate:ctrl_alt_bksp\nchromium-browser --disable-infobars --kiosk '%s'" $2 | sudo tee /etc/xdg/openbox/autostart
 
-sudo apt-get install unclutter -y
+sudo printf "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor" | sudo tee .bash_profile
+
+sudo raspi-config
 
 echo 'Done.'
